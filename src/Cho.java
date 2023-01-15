dce63db7e9dc044b7e3f8daf071c2e1151844b91
DESKTOP-8467F69\ASUS <pilsun9599@gmail.com>
2023-01-15 23:18:05 +0700

    baitap Cho

---
+++ src/Cho.java
@@ -0,0 +1,14 @@
+public class Cho {
+    public String origin;
+    public String furColor;
+    public int age;
+    public float price;
+    public Cho (String origin, String furColor, int age, int price){
+        this.origin = origin;
+        this.furColor = furColor;
+        this.age = age;
+        this.price = price;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int choice;
+        Cho Husky = new Cho("USA", "gray", 4, 1000);
+        Cho Kien = new Cho("VN", "black", 5, 200);
+        Cho Laika = new Cho("Russia", "white", 6, 800);
         Scanner scanner = new Scanner(System.in);
         do {
-            System.out.println("1.Tính chu vi và diện tích hình chu nhật");
-            System.out.println("2.Tính chu vi và diện tích hình tròn");
-            System.out.println("3.Tính chu vi và diện tích hình vuông");
-            System.out.println("4.Thoát chương trình");
+            System.out.println("Chọn chú chó mà bạn muốn mua :");
+            System.out.println("1. Chó Husky");
+            System.out.println("2. Chó Kiến");
+            System.out.println("3. Chó Laika");
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    HinhChuNhat cvdthcn = new HinhChuNhat();
-                    cvdthcn.tinhToan1();
+                    System.out.println("Origin : " + Husky.origin);
+                    System.out.println("Color : " + Husky.furColor);
+                    System.out.println("Age(Months) : " + Husky.age);
+                    System.out.println("Price($) : " + Husky.price);
                     break;
                 case 2:
-                    HinhTron cvdtht = new HinhTron();
-                    cvdtht.tinhtoan2();
+                    System.out.println("Origin : " + Kien.origin);
+                    System.out.println("Color : " + Kien.furColor);
+                    System.out.println("Age(Months) : " + Kien.age);
+                    System.out.println("Price($) : " + Kien.price);
                     break;
                 case 3:
-                    HinhVuong cvdthv = new HinhVuong();
-                    cvdthv.tinhToan3();
+                    System.out.println("Origin : " + Laika.origin);
+                    System.out.println("Color : " + Laika.furColor);
+                    System.out.println("Age(Months) : " + Laika.age);
+                    System.out.println("Price($) : " + Laika.price);
                     break;
             }
-
-        }while (choice!=4);
+        }while (choice!=3);
     }
 }
