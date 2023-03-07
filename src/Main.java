1c4dddbbda992ab7f0b7c2777388628e5d96388c
Nguyen Thanh Luan <luann4099@gmail.com>
2023-03-07 19:26:36 +0700

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * The type Main.
@@ -11,46 +10,19 @@ public class Main {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        ProductManager productManager = new ProductManager();
-        Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        while (choice != 4) {
-            System.out.println("Menu");
-            System.out.println("1. Tìm kiếm bằng tên");
-            System.out.println("2. Hiển thi theo danh sách ");
-            System.out.println("3. Tính tổng tiền hàng");
-            System.out.println("4. Thoát");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("Nhập tên cần tìm");
-                    String nameSearch = scanner.next();
-                    Product product = productManager.findByName(nameSearch);
-                    if (product != null) {
-                        System.out.println(product.getId() + product.getName() + product.getPrice() + product.getQuantity());
-                    } else {
-                        System.out.println("Không tim thấy san pham");
-                    }
-                    break;
-                case 2:
-                    ArrayList<Product> list = productManager.getList();
-                    for (Product product1 : list) {
-                        System.out.println(product1.getId() + " | " + product1.getName() + " | " + product1.getPrice() + " | " + product1.getQuantity());
-                    }
-                    break;
-                case 3:
-                    System.out.println("Nhập ID các sản phẩm cần tính");
-                    int id = scanner.nextInt();
-                    System.out.println("TỔng tiền hàng sp có id " + id + " là: " + productManager.calculateSum(id));
-                    break;
-                case 4:
-                    System.out.println("Thoát");
-                    break;
-                default:
-                    System.out.println("Lựa chọn không đúng");
-                    break;
-            }
-        }
+        //singleton design pattern
+        /*Calendar calc = Calendar.getInstance();
+        System.out.println(calc.get(Calendar.MONTH)+1);
+        System.out.println(calc.get(Calendar.DAY_OF_MONTH));
+        System.out.println(calc.get(Calendar.DAY_OF_WEEK));*/
+        /*Date date = new Date();
+        System.out.println(date.getTime());
+        System.out.println(date.getDate());
+        System.out.println(date.getDay());
+        System.out.println(date.getMonth()+1);*/
 
+        Random random = new Random();
+        //System.out.println(random.nextFloat() * 100);//0-100
+        System.out.println(random.nextFloat() * (100 - 20)+20);//0*80+20=20, 1*80+20=100, 0.5*80+20=60, 0.2*80+20=36
     }
 }
